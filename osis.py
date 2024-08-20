da91ed69335d4f81b9d10f5fc9382f63f211b750
althaf <althafrayhan5@gmail.com>
2024-08-20 14:35:43 +0700

    trouble button & addding bgkoor7

---
+++ osis.py
@@ -0,0 +1,174 @@
+from copy import deepcopy
+import math
+
+
+class Groesse:
+    def __init__(self, formelsymbol, einheit, konst, wert=None):
+        self.formelsymbol = formelsymbol
+        self.einheit = einheit
+        self.wert = wert
+        self.konst = konst
+        self.equals_var = None
+
+    def set(self, wert):
+        self.wert = wert
+
+    def set_equals_var(self, var):
+        self.equals_var = var
+
+
+class Formel:
+    class Solver:
+        def __init__(self, formel):
+            self.formel = formel
+            self.dicts = []
+
+        def set_geg(self):
+            for element in self.dicts[0]:
+                for element1 in self.formel.liste:
+                    if element == element1.formelsymbol:
+                        element1.set(self.dicts[0][element])
+
+        def set_dicts(self, dicts):
+            self.dicts = dicts
+
+        def get_dicts(self):
+            return self.dicts
+
+        def solve_to_i(self):
+            return [(self.formel.a.wert**self.formel.z.wert) * (self.formel.b.wert**self.formel.y.wert)
+                    * (self.formel.c.wert**self.formel.x.wert) + self.formel.d.wert * self.formel.e.wert
+                    * self.formel.f.wert + self.formel.g.wert, self.get_dicts()]
+
+        def solve_to_a(self):
+            return [((self.formel.i.wert - self.formel.d.wert * self.formel.e.wert * self.formel.f.wert
+                      - self.formel.g.wert)/((self.formel.b.wert ** self.formel.y.wert)
+                                             * (self.formel.c.wert ** self.formel.x.wert)))**(1/self.formel.z.wert),
+                    self.get_dicts()]
+
+        def solve_to_b(self):
+            return [((self.formel.i.wert - self.formel.d.wert * self.formel.e.wert * self.formel.f.wert
+                      - self.formel.g.wert)/((self.formel.a.wert ** self.formel.z.wert)
+                                             * (self.formel.c.wert ** self.formel.x.wert)))**(1/self.formel.y.wert),
+                    self.get_dicts()]
+
+        def solve_to_c(self):
+            return [((self.formel.i.wert - self.formel.d.wert * self.formel.e.wert * self.formel.f.wert
+                      - self.formel.g.wert)/((self.formel.b.wert ** self.formel.y.wert)
+                                             * (self.formel.a.wert ** self.formel.z.wert)))**(1/self.formel.x.wert),
+                    self.get_dicts()]
+
+        def solve_to_d(self):
+            return [(self.formel.i.wert - self.formel.g.wert - (self.formel.a.wert**self.formel.z.wert)
+                    * (self.formel.b.wert**self.formel.y.wert) * (self.formel.c.wert**self.formel.x.wert))
+                    / (self.formel.e.wert * self.formel.f.wert), self.get_dicts()]
+
+        def solve_to_e(self):
+            return [(self.formel.i.wert - self.formel.g.wert - (self.formel.a.wert**self.formel.z.wert)
+                    * (self.formel.b.wert**self.formel.y.wert) * (self.formel.c.wert**self.formel.x.wert))
+                    / (self.formel.d.wert * self.formel.f.wert), self.get_dicts()]
+
+        def solve_to_f(self):
+            return [(self.formel.i.wert-self.formel.g.wert-(self.formel.a.wert**self.formel.z.wert)
+                     * (self.formel.b.wert**self.formel.y.wert) * (self.formel.c.wert**self.formel.x.wert))
+                    / (self.formel.d.wert * self.formel.e.wert), self.get_dicts()]
+
+        def solve_to_g(self):
+            return [- ((self.formel.a.wert**self.formel.z.wert) * (self.formel.b.wert**self.formel.y.wert)
+                       * (self.formel.c.wert**self.formel.x.wert) + self.formel.d.wert * self.formel.e.wert
+                       * self.formel.f.wert - self.formel.i.wert), self.get_dicts()]
+
+        def solve_to_z(self):
+            return [math.log((self.formel.i.wert-self.formel.d.wert*self.formel.e.wert
+                              * self.formel.f.wert-self.formel.g.wert) / ((self.formel.b.wert**self.formel.y.wert)
+                                                                          * (self.formel.c.wert**self.formel.x.wert)),
+                             self.formel.a.wert), self.get_dicts()]
+
+        def solve_to_y(self):
+            return [math.log((self.formel.i.wert-self.formel.d.wert*self.formel.e.wert
+                              * self.formel.f.wert-self.formel.g.wert)/((self.formel.a.wert**self.formel.z.wert)
+                                                                        * (self.formel.c.wert**self.formel.x.wert)),
+                             self.formel.b.wert), self.get_dicts()]
+
+        def solve_to_x(self):
+            return [math.log((self.formel.i.wert-self.formel.d.wert*self.formel.e.wert
+                             * self.formel.f.wert-self.formel.g.wert)/((self.formel.a.wert**self.formel.z.wert)
+                                                                       * (self.formel.b.wert**self.formel.y.wert)),
+                             self.formel.c.wert), self.get_dicts()]
+
+    def __init__(self, name, grundform, i, a=Groesse(None, None, True, 0), b=Groesse(None, None, True, 0),
+                 c=Groesse(None, None, True, 0), d=Groesse(None, None, True, 0), e=Groesse(None, None, True, 0),
+                 f=Groesse(None, None, True, 0), g=Groesse(None, None, True, 0), z=Groesse(None, None, True, 0),
+                 y=Groesse(None, None, True, 0), x=Groesse(None, None, True, 0)):
+        self.a = a
+        self.a.set_equals_var('a')
+        self.b = b
+        self.b.set_equals_var('b')
+        self.c = c
+        self.c.set_equals_var('c')
+        self.d = d
+        self.d.set_equals_var('d')
+        self.e = e
+        self.e.set_equals_var('e')
+        self.f = f
+        self.f.set_equals_var('f')
+        self.g = g
+        self.g.set_equals_var('g')
+        self.z = z
+        self.z.set_equals_var('z')
+        self.y = y
+        self.y.set_equals_var('y')
+        self.x = x
+        self.x.set_equals_var('x')
+        self.name = name
+        self.i = i
+        self.i.set_equals_var('i')
+        self.liste = []
+        self.string = grundform
+        self.solver = self.Solver(self)
+        for var in [self.a, self.b, self.c, self.d, self.e, self.f, self.g, self.z, self.y, self.x, self.i]:
+            if var.konst is False:
+                self.liste.append(var)
+
+    def is_solvable(self, geg, ges):
+        temp_liste = deepcopy(self.liste)
+        for geg_element in geg:
+            for i, liste_element in enumerate(temp_liste):
+                if geg_element == liste_element.formelsymbol:
+                    temp_liste.pop(i)
+        if len(temp_liste) == 1 and list(ges.keys())[0] == temp_liste[0].formelsymbol:
+            return [True, temp_liste[0]]
+        return [False, None]
+
+    def solve(self, ges, dicts):
+        temp_dicts = dicts
+        temp_dicts.append(self)
+        ret = None
+        self.solver.set_dicts(temp_dicts)
+        self.solver.set_geg()
+        if 'i' == ges.equals_var:
+            ret = self.solver.solve_to_i()
+        elif 'e' == ges.equals_var:
+            ret = self.solver.solve_to_e()
+        elif 'c' == ges.equals_var:
+            ret = self.solver.solve_to_c()
+        elif 'b' == ges.equals_var:
+            ret = self.solver.solve_to_b()
+        elif 'a' == ges.equals_var:
+            ret = self.solver.solve_to_a()
+        elif 'x' == ges.equals_var:
+            ret = self.solver.solve_to_x()
+        elif 'd' == ges.equals_var:
+            ret = self.solver. solve_to_d()
+        elif 'e' == ges.equals_var:
+            ret = self.solver. solve_to_e()
+        elif 'f' == ges.equals_var:
+            ret = self.solver. solve_to_f()
+        elif 'g' == ges.equals_var:
+            ret = self.solver. solve_to_g()
+        elif 'y' == ges.equals_var:
+            ret = self.solver. solve_to_y()
+        elif 'z' == ges.equals_var:
+            ret = self.solver. solve_to_z()
+
+        return ret
--- script.js
+++ script.js
@@ -167,3 +167,262 @@ form.addEventListener("submit", (e) => {
 alertCloseBtn.addEventListener("click", () => {
   alertModal.close();
 });
+// FALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSEFALSE
+//  https://www.toptal.com/developers/keycode
+const world = document.getElementById("world");
+const container = document.getElementById("container");
+const infoWindow = document.getElementById("infoWindow");
+
+const coin = document.getElementById("euro");
+const coin2 = document.getElementById("euro2");
+const saule = document.getElementById("saule");
+
+const deg = Math.PI / 180;
+const accselFreeFall = 9.8066;
+
+document.addEventListener("keydown", onKeyPress);
+document.addEventListener("keyup", onKeyRelese);
+document.addEventListener("mousemove", onMouseMove);
+
+document.addEventListener("pointerlockchange", () => {
+  lockedPointer = !lockedPointer;
+});
+
+container.onclick = function () {
+  container.requestPointerLock();
+};
+
+let lockedPointer = false;
+let isInfoPanelOpen = false;
+
+let movementSpeed = 6;
+let sensitivity = 0.2;
+
+let defaultPlayerHeight = 410;
+let playerHeight = defaultPlayerHeight;
+
+let cameraCrouch = 0.75;
+let isCrouching = false;
+
+let jumpHeight = -9.15;
+let fallVelocity = 0;
+let time = Date.now();
+
+let position = vec3(500, defaultPlayerHeight, 0);
+let rotation = vec3(-30, 45, 0);
+
+let cointRotY = 0;
+
+//Util function
+function vec3(x = 0, y = 0, z = 0) {
+  return {
+    x: x,
+    y: y,
+    z: z,
+  };
+}
+
+//Event callbacks
+let keymap = {
+  KeyA: false,
+  KeyD: false,
+  KeyW: false,
+  KeyS: false,
+  KeyQ: false,
+  KeyE: false,
+  ShiftLeft: false,
+  KeyZ: false,
+  ControlLeft: false,
+};
+
+function onKeyPress(event) {
+  if (keymap[event.code] != null) {
+    keymap[event.code] = true;
+  }
+}
+function onKeyRelese(event) {
+  if (event.code == "KeyC") {
+    isCrouching = !isCrouching;
+  }
+
+  if (event.code == "Space" && !isCrouching) {
+    fallVelocity = jumpHeight;
+  }
+
+  if (event.code == "KeyI") {
+    let newS = prompt("Cik jutigu velies peliti?");
+
+    if (parseFloat(newS) && !isNaN(parseFloat(newS))) {
+      sensitivity = parseFloat(newS) / 100;
+    }
+
+    return;
+  } else if (event.code == "KeyR") {
+    cameraHeight = 410;
+    position = vec3(500, 360, 0);
+    rotation = vec3(-30, 0, 0);
+
+    return;
+  }
+
+  if (keymap[event.code] != null) {
+    keymap[event.code] = false;
+  }
+}
+function onMouseMove(event) {
+  if (!lockedPointer) return;
+
+  rotation.y += event.movementX * sensitivity;
+  rotation.x -= event.movementY * sensitivity;
+}
+
+//Update functions
+
+function updateWorld() {
+  world.style.transform = `translateZ( 600px ) rotateX( ${rotation.x}deg ) rotateY( ${rotation.y}deg ) translate3d(${position.x}px, ${position.y}px, ${position.z}px)`;
+
+  coin.style.transform = `translate3d(200px, 100px, 100px) rotateX(0deg) rotateY(${cointRotY}deg) rotateZ(0deg)`;
+  coin2.style.transform = `translate3d(-100px, 100px, 100px) rotateX(0deg) rotateY(${cointRotY}deg) rotateZ(0deg)`;
+  saule.style.transform = `translate3d(0px, -1000px, 0px) rotatex(${
+    rotation.x
+  }deg) rotateY(${0 - rotation.y}deg) rotateZ(${0 - rotation.z}deg)`;
+}
+
+function updatePlayerMovement() {
+  let facingVector = vec3(
+    Math.cos(rotation.y * deg),
+    Math.sin(rotation.y * deg)
+  );
+
+  if (keymap.KeyW) {
+    position.z += facingVector.x * movementSpeed;
+    position.x -= facingVector.y * movementSpeed;
+  }
+  if (keymap.KeyS) {
+    position.z -= facingVector.x * movementSpeed;
+    position.x += facingVector.y * movementSpeed;
+  }
+
+  if (keymap.KeyA) {
+    position.x += facingVector.x * movementSpeed;
+    position.z += facingVector.y * movementSpeed;
+  }
+  if (keymap.KeyD) {
+    position.x -= facingVector.x * movementSpeed;
+    position.z -= facingVector.y * movementSpeed;
+  }
+
+  if (rotation.x > 360) {
+    rotation.x -= 360;
+  }
+  if (rotation.x < -360) {
+    rotation.x += 360;
+  }
+
+  if (rotation.y > 360) {
+    rotation.y -= 360;
+  }
+  if (rotation.y < -360) {
+    rotation.y += 360;
+  }
+
+  if (rotation.z > 360) {
+    rotation.z -= 360;
+  }
+  if (rotation.z < -360) {
+    rotation.z += 360;
+  }
+}
+
+function cameraJump() {
+  seconds = Math.min((Date.now() - time) / 1000, 0.1);
+  time = Date.now();
+
+  fallVelocity = fallVelocity + accselFreeFall * seconds;
+  position.y = position.y - fallVelocity;
+
+  if (position.y <= playerHeight) {
+    position.y = playerHeight;
+  }
+
+  if (isCrouching) {
+    if (playerHeight > defaultPlayerHeight * cameraCrouch) {
+      playerHeight = playerHeight * cameraCrouch;
+    }
+  } else if (!isCrouching && playerHeight < defaultPlayerHeight) {
+    playerHeight = playerHeight / cameraCrouch;
+  }
+}
+
+function drawInfoPanel() {
+  if (!isInfoPanelOpen) {
+    container.style.width = `98%`;
+
+    infoWindow.style.width = `0%`;
+    infoWindow.style.marginRight = `0%`;
+    infoWindow.style.marginLeft = `0%`;
+    infoWindow.style.padding = `0%`;
+
+    infoWindow.innerHTML = ``;
+  } else if (isInfoPanelOpen) {
+    container.style.width = `84%`;
+
+    infoWindow.style.width = `10%`;
+    infoWindow.style.marginRight = `2%`;
+    infoWindow.style.marginLeft = `1%`;
+    infoWindow.style.padding = `1%`;
+
+    infoWindow.innerHTML = `
+        <h5>The Information Panel</h5>
+        <hr>
+        <p>Movement speed: ${movementSpeed}px
+        Camera sensitivity: ${sensitivity * 100}%
+        <hr>
+        Players camera coordinates:<br>
+        x: ${position.x}<br>
+        y: ${position.y}<br>
+        z: ${position.z}
+        <hr>
+        
+        Players camera rotation:<br>
+        
+        x: ${rotation.x}<br>
+        y: ${rotation.y}<br>
+        z: ${rotation.z}
+
+        <hr>
+        </p>
+        <p id="finePrint">
+        Movement controls:<br>
+        w: move forward<br>
+        a: move left<br>
+        s: move backward<br>
+        d: move right<br>
+        
+        shift: sprint<br>
+        <hr>
+        <p id="finePrint">Other functions:<br>
+        r: reset back to starting position<br>
+        i: open & close the information panel<br>
+        </p>`;
+  }
+}
+
+//Main game loop
+function game() {
+  cointRotY += 5;
+  if (cointRotY > 360) {
+    cointRotY -= 360;
+  }
+
+  updatePlayerMovement();
+  cameraJump();
+
+  drawInfoPanel();
+  updateWorld();
+
+  myReq = requestAnimationFrame(game);
+}
+
+//Starting le gaem
+game();
